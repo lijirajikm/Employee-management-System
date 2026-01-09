6b3fb0e3970397c457c1cc0e64f2f99ffa84775d
lijiraj@ikm.gov.in <lijiraj@ikm.gov.in>
2026-01-09 16:02:57 +0530

    Rename IdExistException to EmpCodeExistExeption

--- exam/src/main/java/com/ksmart/exam/exception/IdExistException.java
+++ exam/src/main/java/com/ksmart/exam/exception/IdExistException.java
@@ -1,7 +1,7 @@
 package com.ksmart.exam.exception;
 
-public class IdExistException extends RuntimeException {
-    public IdExistException(String message) {
+public class EmpCodeExistExeption extends RuntimeException {
+    public EmpCodeExistExeption(String message) {
         super(message);
     }
 }
