c64609b7c16992dfd8be70d4048282fc923aae6f
lijiraj@ikm.gov.in <lijiraj@ikm.gov.in>
2026-01-09 15:38:59 +0530

    Update EmployeeRequest.java

--- exam/src/main/java/com/ksmart/exam/contract/EmployeeRequest.java
+++ exam/src/main/java/com/ksmart/exam/contract/EmployeeRequest.java
@@ -13,11 +13,17 @@ import lombok.Setter;
 public class EmployeeRequest {
     private UUID id;
     @NotBlank(message = "Employee code is required")
-    
     private String employeeCode;
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Department is required")
     private String department;
+    @NotBlank(message = "Salary is required")
     private String salary;
+    @Pattern(
+   regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
+   message = "Email must be valid"
+)
     private String email;
 }
 
